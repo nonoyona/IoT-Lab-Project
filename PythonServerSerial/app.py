2acee94a963a7334fc8f6aa1cf75696510af3a15
Noah- Yannick Schmid <44346907+nonoyona@users.noreply.github.com>
2023-08-16 16:20:54 +0200

    Update app.py

--- PythonServerSerial/app.py
+++ PythonServerSerial/app.py
@@ -1,31 +1,21 @@
-from flask import Flask, jsonify
+import csv
+import time
 from serial_interface import SerialInterface
-import threading
 
-app = Flask(__name__)
-serial_interface = SerialInterface('/dev/ttyUSB0', 9600)
+def main():
+    port = "COM1"  # Replace with your actual serial port
+    baudrate = 9600  # Replace with the appropriate baudrate
 
-# Variable to store the most recent data from the SerialInterface
-latest_data = {}
+    with SerialInterface(port, baudrate) as serial_interface:
+        with open('data.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Timestamp', 'Data'])
 
-# Function to continuously update the latest_data variable with the most recent data
-def update_data():
-    global latest_data
-    while True:
-        data = serial_interface.read()
-        latest_data = data
+            while True:
+                data = serial_interface.read()
+                timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+                csv_writer.writerow([timestamp, str(data)])
+                csv_file.flush()  # Flush to ensure data is written immediately
 
-# Create and start a separate thread for updating the latest_data
-update_thread = threading.Thread(target=update_data)
-update_thread.daemon = True
-update_thread.start()
-
-@app.route('/data', methods=['GET'])
-def get_latest_data():
-    return jsonify(latest_data)
-
-if __name__ == '__main__':
-    try:
-        app.run(host='0.0.0.0', port=5000)
-    except KeyboardInterrupt:
-        serial_interface.close()
+if __name__ == "__main__":
+    main()
